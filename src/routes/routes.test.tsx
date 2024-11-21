e30f22314aea1d234cee809adeb720113d7f88a2
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-21 20:43:47 -0300

    Dashboartd

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,8 @@
-import { render, waitFor, screen } from "@__tests__/utils/customRender"
+import { render, waitFor, screen, act } from "@__tests__/utils/customRender"
 import { Routes } from "."
+import { api } from "@services/api"
+import { mockWeatherAPIResponse } from "@__tests__/mocks/mockWheaterAPIResponse"
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
 
 describe("Routes: Routes", () => {
   it("should be rendered Search screen when no city is defined", async () => {
@@ -10,9 +13,20 @@ describe("Routes: Routes", () => {
   })
 
   it("should be rendered Dashboard screen when a city is defined", async () => {
-    render(<Routes />)
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse })
 
-    const title = await waitFor(() => screen.getByText(/^escolha um local/i))
+    const city = {
+      id: "1",
+      name: "Campinas",
+      latitude: 123,
+      longitude: 456,
+    }
+
+    await saveStorageCity(city)
+
+    await act(() => waitFor(() => render(<Routes />)))
+
+    const title = screen.getByText(city.name)
     expect(title).toBeTruthy()
   })
 })
