a736b613c36c6905ed2452d9ddfda236101604a9
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-21 19:10:22 -0300

    Async Storage Mock

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,25 @@
-import { getStorageCity } from "./cityStorage"
+import { CityProps } from "@services/getCityByNameService"
+import { getStorageCity, saveStorageCity } from "./cityStorage"
 
 describe("Storage: cityStorage", () => {
   it("should return null when storage is empty", async () => {
     const response = await getStorageCity()
 
     expect(response).toBeNull()
+  })
+
+  it("should return stored city", async () => {
+    const city: CityProps = {
+      id: "1",
+      name: "Campinas",
+      latitude: 123,
+      longitude: 456,
+    }
+
+    await saveStorageCity(city)
+
+    const response = await getStorageCity()
+
+    expect(response).toEqual(city)
   })
 })
