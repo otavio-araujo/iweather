aa3df19f54dcf23b3610b86e485c2b1c2d77df1e
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-21 18:29:52 -0300

    Function mock

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -4,13 +4,16 @@ import { SelectList } from "."
 describe("Component: SelcetList", () => {
   it("should return selected city details", () => {
     const data = [
-      { id: 1, name: "Campinas", latitude: 123, longitude: 456 },
-      { id: 2, name: "Campo Grande", latitude: 789, longitude: 123 },
+      { id: "1", name: "Campinas", latitude: 123, longitude: 456 },
+      { id: "2", name: "Campo Grande", latitude: 789, longitude: 123 },
     ]
-    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />)
 
-    const selectedCity = screen.getByText(/campo/i)
+    const onPress = jest.fn()
+
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />)
+    const selectedCity = screen.getByText("Campinas")
 
     fireEvent.press(selectedCity)
+    expect(onPress).toHaveBeenCalled()
   })
 })
