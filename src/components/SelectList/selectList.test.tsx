91d3df7f78c12523cc4310c21d2edcf64a990483
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-21 17:51:14 -0300

    getByText

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react-native"
+import { SelectList } from "."
+
+describe("Component: SelcetList", () => {
+  it("should return selected city details", () => {
+    const data = [
+      { id: 1, name: "Campinas", latitude: 123, longitude: 456 },
+      { id: 2, name: "Campo Grande", latitude: 789, longitude: 123 },
+    ]
+    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />)
+
+    const selectedCity = screen.getByText(/campo/i)
+  })
+})
